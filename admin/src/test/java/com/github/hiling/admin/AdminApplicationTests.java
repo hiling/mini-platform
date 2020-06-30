bce5f9cc77e8dd20783fb76bcb61e88cc2d44c68
hiling <hiling@outlook.com>
2020-06-30 13:05:13 +0800

    集成SpringBoot Admin

---
+++ admin/src/test/java/com/github/hiling/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hiling.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
